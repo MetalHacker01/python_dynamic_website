df9eedfa718fd9b736322caaa9d71742a45f8f38
Aldorino Rrushi <MetalHacker01@gmail.com>
2023-02-23 11:02:53 +0000

    functionality change

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 jobs = [{
   'id': 1,
+  'img':
+  "https://images.unsplash.com/photo-1618477388954-7852f32655ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
   'title': 'Web Developer',
   "region": 'Europe',
   "salary": 'Starting from 5000$',
@@ -11,6 +13,8 @@ jobs = [{
   'working-hours': 'Part Time'
 }, {
   'id': 2,
+  'img':
+  "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
   'title': 'Data Analyst',
   'region': 'North America',
   'salary': 'Starting from 5500$',
@@ -18,10 +22,21 @@ jobs = [{
   'working-hours': 'Full Time'
 }, {
   'id': 3,
+  'img':
+  "https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
   'title': 'Mkt Manager',
   'region': 'North America',
   'seniority': 'Field Expert',
   'working-hours': 'Full Time'
+}, {
+  'id': 4,
+  'img':
+  "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+  'title': 'Marketing Coordinator',
+  "region": 'North America',
+  "salary": 'Starting from 4000$',
+  'seniority': 'Entry Level',
+  'working-hours': 'Part Time'
 }]
 
 
